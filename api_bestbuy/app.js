63a984de08e23dff51c24eab21bbbc4f86837237
Luis Gonzalez <lmgl887@gmail.com>
2022-05-19 19:50:39 -0500

    api works, miss delete

--- api_bestbuy/app.js
+++ api_bestbuy/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const { nanoid } = require('nanoid');
 
 
 
@@ -21,15 +22,15 @@ let connection = mysql.createConnection({
 });
 
 //get all products
-app.get('/products', (req, res)=>{
+app.get('/products', (req, res) => {
 
   const getAllQUERY = "SELECT * FROM products";
 
-  connection.query(getAllQuery, (err, results)=>{
-    if(err) throw err
-    if (results.length > 0){
+  connection.query(getAllQUERY, (err, results) => {
+    if (err) throw err
+    if (results.length > 0) {
       res.json(results);
-    }else{
+    } else {
       res.send('No results')
     }
   })
@@ -38,42 +39,81 @@ app.get('/products', (req, res)=>{
 
 //get by title
 app.get('/products/:title', (req, res) => {
-  const {title} = req.params
+  const { title } = req.params
   const getByTitleQUERY = `SELECT * FROM products WHERE title LIKE '${title}%'`
 
 
-  connection.query(getByTitleQUERY, (err, result)=>{
-    if(err) throw error;
+  connection.query(getByTitleQUERY, (err, result) => {
+    if (err) throw error;
 
-    if(result.length > 0){
+    if (result.length > 0) {
       res.json(result);
-    }else{
+    } else {
       res.send('No result')
     }
   })
 
 })
 
+//get by category
+app.get('/products/cat/:category', (req, res) => {
+  const { category } = req.params
+  const getByCategoryQUERY = `SELECT * FROM products WHERE category LIKE '${category}'`
+
+
+  connection.query(getByCategoryQUERY, (err, result) => {
+    if (err) throw error;
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('No result')
+    }
+  })
+
+})
+
+
+
 //create product
-app.post('/products/new',(req, res)=>{
+app.post('/products/new', (req, res) => {
+  const addNewQUERY = "INSERT INTO products SET ?";
+
+  const product = {
+    id: nanoid(5),
+    image: req.body.image,
+    title: req.body.title,
+    modelTag: req.body.modelTag,
+    rating: req.body.rating,
+    category: req.body.category,
+    brand: req.body.brand,
+    price: req.body.price,
+    isPromo: req.body.isPromo,
+
+  }
+
+  connection.query(addNewQUERY, product, error => {
+    if (error) throw error;
+
+    res.send('product added!')
+  })
+})
+
+
+app.put('/products/get/:id', (req, res) => {
+
+})
+
+app.delete('/products/del/:id', (req, res) => {
 
 })
 
 
-app.put('/products/get/:id', (req, res)=>{
-
-})
-
-app.delete('/products/del/:id', (req, res)=>{
-
-})
-
-
-connection.connect(error =>{
-  if(error) throw error;
+connection.connect(error => {
+  if (error) throw error;
   console.log('Database server running')
 });
 
-app.listen(PORT, ()=>{console.log(`Server running on port ${PORT}`)});
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) });
 
 
