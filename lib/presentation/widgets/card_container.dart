2b2e0348fc61862ef51c7f1429c70f819061736f
Luis GL <lmgl887@gmail.com>
2023-03-23 16:32:44 -0600

    update cardContainer height & top-padding

--- lib/presentation/widgets/card_container.dart
+++ lib/presentation/widgets/card_container.dart
@@ -1,35 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CardContainer extends StatelessWidget {
-  const CardContainer({required this.child, Key? key }) : super(key: key);
+  const CardContainer({required this.child, Key? key}) : super(key: key);
 
   final Widget? child;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       child: Container(
         width: double.infinity,
         padding: EdgeInsets.all(20),
-        height: 750,
+        height: 700,
         decoration: _createCardShape(),
         child: this.child,
       ),
     );
   }
 
-
-  BoxDecoration _createCardShape()=> BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.circular(25),
-    
-    boxShadow: [
-      BoxShadow(
-      color: Colors.black12,
-      blurRadius: 15,
-      offset: Offset(0,5)
-    )
-    ]
-  );
+  BoxDecoration _createCardShape() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, blurRadius: 15, offset: Offset(0, 5))
+          ]);
 }
